d255fb51b40be8c21f3f927e18e55bc64ef2253a
Vitaliy Shapovalov <kupper133@gmail.com>
2021-01-24 01:02:55 +0300

    update #4 issue, continue working on the side menu, add container view for adding to other view of application and describe toggleMenu protocol

---
+++ Sushiwok/Controller/MenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MenuViewController.swift
+//  Sushiwok
+//
+//  Created by Виталий Шаповалов on 23.01.2021.
+//  Copyright © 2021 Виталий Шаповалов. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
