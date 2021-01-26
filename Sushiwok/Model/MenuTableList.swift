8e59e5aebc742eba5b6973224620d8eb4a248110
Vitaliy Shapovalov <kupper133@gmail.com>
2021-01-27 01:11:16 +0300

    -refactor naming of variables -custom my menu view controller, add logo and table menu -create menu items
    update #12 issue, was spent 2h 30m

---
+++ Sushiwok/Model/MenuTableList.swift
@@ -0,0 +1,31 @@
+//
+//  MenuTableList.swift
+//  Sushiwok
+//
+//  Created by Виталий Шаповалов on 27.01.2021.
+//  Copyright © 2021 Виталий Шаповалов. All rights reserved.
+//
+
+import UIKit
+
+struct MenuTableList {
+    var menuItemArray = [String]()
+    var menuIconsArray = [UIImage]()
+    
+    init(menuItemArray:[String], menuIconsArray:[UIImage]) {
+        self.menuItemArray = menuItemArray
+        self.menuIconsArray = menuIconsArray
+    }
+}
+
+func createMenuItems()->MenuTableList{
+    return MenuTableList(
+        menuItemArray:["Menu", "Deals and Sales", "Favorites", "Our Shops"],
+        menuIconsArray:[
+            UIImage(systemName: "book.fill")!,
+            UIImage(systemName: "bubble.left.and.bubble.right.fill")!,
+            UIImage(systemName: "location.fill")!,
+            UIImage(systemName: "heart.fill")!
+        ]
+    )
+}
