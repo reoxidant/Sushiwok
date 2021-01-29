4f117966f5945958358047eb779e537748e40327
Vitaliy Shapovalov <kupper133@gmail.com>
2021-01-30 00:08:47 +0300

    create UISegmentControll and bind each tab with UITableView - fill category for sets 5 items and fill category for rolls 5 items

--- Sushiwok/Model/ListItem.swift
+++ Sushiwok/Model/ListItem.swift
@@ -14,21 +14,25 @@ struct ListItem {
     var title:String
     var description:String
     
-    static func createSushiArray() -> [ListItem]{
-        var tempArray = [ListItem]()
+    static func createSushiArray() -> [[ListItem]]{
+        let categories =
+            [
+                [
+                    ListItem(image: #imageLiteral(resourceName: "fila-bum"), title: "Фила Бум", description: "ролл Филадельфия 2 шт., ролл Филадельфия в угре, ролл Филадельфия в масаго, ролл Фудживара 1031 г"),
+                    ListItem(image: #imageLiteral(resourceName: "umka"), title: "Умка", description: "ролл Фиеста, ролл Сэнсей, ролл Калифорния в кунжуте 653 г"),
+                    ListItem(image: #imageLiteral(resourceName: "filomaniya"), title: "Филомания", description: "ролл Филадельфия в масаго, ролл Филадельфия, ролл Калифорния в кунжуте, ролл с огурцом 707 г"),
+                    ListItem(image: #imageLiteral(resourceName: "uikend"), title: "Уикенд", description: "ролл Сочная креветка, ролл Хатамото, запеч. ролл Окунь-гриль, запеч. ролл Крабик Хот, ролл Калифорния с креветкой, ролл Марокко, запеч. ролл Румяный, запеч. ролл Сырный, ролл Фудживара 1707 г"),
+                    ListItem(image: #imageLiteral(resourceName: "iskushenie"), title: "Набор Искушение", description: "ролл Ореховый рай, ролл Берри, ролл Сочный фрукт 438 г")
+                ],
+                [
+                    ListItem(image: #imageLiteral(resourceName: "midori"), title: "Ролл Окитори", description: "лосось, цыпленок, омлет тамаго, масаго, шеф-соус, спайси соус, кунжут 234 г"),
+                    ListItem(image: #imageLiteral(resourceName: "ebi-roru"), title: "Ролл Эби Рору", description: "креветка тигровая, краб-крем, огурец, перец болгарский, салат айсберг, масаго, медовый соус 252 г"),
+                    ListItem(image: #imageLiteral(resourceName: "midori"), title: "Ролл Спайси креветка", description: "лосось, сливочный сыр, огурец, масаго, креветки,s спайси соус 217 г"),
+                    ListItem(image: #imageLiteral(resourceName: "dabl-unagi"), title: "Ролл Дабл унаги", description: "угорь, цыпленок, краб-крем, сливочный сыр, омлет тамаго, апельсиновый соус, спайси соус, паприка, кунжут 234 г"),
+                    ListItem(image: #imageLiteral(resourceName: "midori"), title: "Ролл Мидори", description: "креветка тигровая, краб-крем, чука, масаго, яки соус, спайси соус, медовый соус 237 г")
+                ]
+        ]
         
-        let sushi1 = ListItem(image: #imageLiteral(resourceName: "sushi1"), title: "Фила Бум", description: "ролл Филадельфия 2 шт., ролл Филадельфия в угре, ролл Филадельфия в масаго, ролл Фудживара 1031 г")
-        let sushi2 = ListItem(image: #imageLiteral(resourceName: "sushi2"), title: "Умка", description: "ролл Фиеста, ролл Сэнсей, ролл Калифорния в кунжуте 653 г")
-        let sushi3 = ListItem(image: #imageLiteral(resourceName: "sushi1"), title: "Филомания", description: "ролл Филадельфия в масаго, ролл Филадельфия, ролл Калифорния в кунжуте, ролл с огурцом 707 г")
-        let sushi4 = ListItem(image: #imageLiteral(resourceName: "sushi4"), title: "Уикенд", description: "ролл Сочная креветка, ролл Хатамото, запеч. ролл Окунь-гриль, запеч. ролл Крабик Хот, ролл Калифорния с креветкой, ролл Марокко, запеч. ролл Румяный, запеч. ролл Сырный, ролл Фудживара 1707 г")
-        let sushi5 = ListItem(image: #imageLiteral(resourceName: "sushi3"), title: "Набор Искушение", description: "ролл Ореховый рай, ролл Берри, ролл Сочный фрукт 438 г")
-        
-        tempArray.append(sushi1)
-        tempArray.append(sushi2)
-        tempArray.append(sushi3)
-        tempArray.append(sushi4)
-        tempArray.append(sushi5)
-        
-        return tempArray
+        return categories
     }
 }
