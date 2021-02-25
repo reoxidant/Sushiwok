638ff0bff81fb2f83e26046f8d5e6ff46ae822d1
Vitaliy Shapovalov <kupper133@gmail.com>
2021-02-25 23:30:44 +0300

    update my sw reveal view controller
    - fix able to get any action on front view, when u see a menu table view

--- Sushiwok/Controllers/MenuListScreen.swift
+++ Sushiwok/Controllers/MenuListScreen.swift
@@ -11,7 +11,7 @@ import SWRevealViewController
 import SwipeMenuViewController
 
 class MenuListScreen: SwipeMenuViewController {
-
+    
     @IBOutlet weak var menuBtn: UIBarButtonItem!
     
     private let categoryMenuList = ["Наборы и комбо", "Роллы", "Wok", "Суши", "Pizza"]
@@ -29,12 +29,11 @@ class MenuListScreen: SwipeMenuViewController {
         
         super.viewDidLoad()
         
-        if(revealViewController() != nil){
-            view.addGestureRecognizer(revealViewController().tapGestureRecognizer())
-            view.addGestureRecognizer(revealViewController().panGestureRecognizer())
-            self.navigationItem.leftBarButtonItem?.target = revealViewController()
+        if let revealController = self.revealViewController() {
+            revealController.panGestureRecognizer()
+            revealController.tapGestureRecognizer()
+            self.navigationItem.leftBarButtonItem?.target = revealController
             self.navigationItem.leftBarButtonItem?.action = #selector(SWRevealViewController.revealToggle(_:))
-            self.revealViewController()?.delegate = self
         }
     }
     
@@ -42,57 +41,32 @@ class MenuListScreen: SwipeMenuViewController {
     override func swipeMenuView(_ swipeMenuView: SwipeMenuView, viewWillSetupAt currentIndex: Int) {
         super.swipeMenuView(swipeMenuView, viewWillSetupAt: currentIndex)
     }
-
+    
     override func swipeMenuView(_ swipeMenuView: SwipeMenuView, viewDidSetupAt currentIndex: Int) {
         super.swipeMenuView(swipeMenuView, viewDidSetupAt: currentIndex)
     }
-
+    
     override func swipeMenuView(_ swipeMenuView: SwipeMenuView, willChangeIndexFrom fromIndex: Int, to toIndex: Int) {
         super.swipeMenuView(swipeMenuView, willChangeIndexFrom: fromIndex, to: toIndex)
     }
-
+    
     override func swipeMenuView(_ swipeMenuView: SwipeMenuView, didChangeIndexFrom fromIndex: Int, to toIndex: Int) {
         super.swipeMenuView(swipeMenuView, didChangeIndexFrom: fromIndex, to: toIndex)
     }
-
-
+    
+    
     // MARK - SwipeMenuViewDataSource
     override func numberOfPages(in swipeMenuView: SwipeMenuView) -> Int {
         return categoryMenuList.count
     }
-
+    
     override func swipeMenuView(_ swipeMenuView: SwipeMenuView, titleForPageAt index: Int) -> String {
         return children[index].title ?? ""
     }
-
+    
     override func swipeMenuView(_ swipeMenuView: SwipeMenuView, viewControllerForPageAt index: Int) -> UIViewController {
         let vc = children[index]
         vc.didMove(toParent: self)
         return vc
-    }
-}
-
-//MARK: - SWRevealController delegates
-extension MenuListScreen : SWRevealViewControllerDelegate{
-    
-    //varying alpha of overlayView with progress of panGesture to reveal or hide menu view
-    func revealController(_ revealController: SWRevealViewController!, panGestureMovedToLocation location: CGFloat, progress: CGFloat) {
-        print("alpha = progress")
-    }
-    
-    //animating alpha in case user just taps hamburger menu which directly change FrontViewPosition
-    func revealController(_ revealController: SWRevealViewController!, animateTo position: FrontViewPosition) {
-        
-        //menu view is hidden
-        if position == FrontViewPosition.left{
-            revealController.view.alpha = 1
-            print("alpha = 0")
-        }
-
-        //menu view is revealed
-        if position == FrontViewPosition.right{
-            revealController.view.alpha = 0.5
-            print("alpha = 1")
-        }
     }
 }
