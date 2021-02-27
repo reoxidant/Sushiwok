3643e55d09c383756a02fdaec1a321b4f7d14d84
Vitaliy Shapovalov <kupper133@gmail.com>
2021-02-27 15:37:12 +0300

    update 23 issue
    - add new pod parchment
    - add uiviewcontrollerins one view pagingviewcontroller
    - comment old code

--- Sushiwok/Controllers/MenuListScreen.swift
+++ Sushiwok/Controllers/MenuListScreen.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 import SWRevealViewController
-import SwipeMenuViewController
+//import SwipeMenuViewController
+import Parchment
 
-class MenuListScreen: SwipeMenuViewController {
+class MenuListScreen: PagingViewController {
     
     @IBOutlet weak var menuBtn: UIBarButtonItem!
     
@@ -18,68 +19,76 @@ class MenuListScreen: SwipeMenuViewController {
     
     override func viewDidLoad() {
         
+        var viewControllers = [UIViewController]()
+        
         for (index, categoryTitle) in categoryMenuList.enumerated() {
             let vc = ContentMenuViewController()
             vc.tableViewPage = index
             vc.title = categoryTitle
-            self.addChild(vc)
+            viewControllers.append(vc)
         }
         
         super.viewDidLoad()
         
-        var options: SwipeMenuViewOptions = .init()
+            let pagingViewController = PagingViewController(viewControllers: viewControllers)
+            addChild(pagingViewController)
+            
+            view.addSubview(pagingViewController.view)
         
-        options.tabView.addition = .circle
-        options.tabView.margin = 8
-        options.tabView.height = 45
-        options.tabView.itemView.selectedTextColor = .white
-        options.tabView.itemView.font = UIFont.systemFont(ofSize: 16)
-        options.tabView.itemView.margin = 8
-        options.tabView.itemView.textColor = .black
-        options.tabView.additionView.circle.cornerRadius = 8
-        options.tabView.additionView.padding = UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 0)
-        options.tabView.additionView.backgroundColor = #colorLiteral(red: 0.6156862745, green: 0.8039215686, blue: 0.168627451, alpha: 1)
+//        var options: SwipeMenuViewOptions = .init()
+//
+//        options.tabView.addition = .circle
+//        options.tabView.margin = 8
+//        options.tabView.height = 45
+//        options.tabView.itemView.selectedTextColor = .white
+//        options.tabView.itemView.font = UIFont.systemFont(ofSize: 16)
+//        options.tabView.itemView.margin = 8
+//        options.tabView.itemView.textColor = .black
+//        options.tabView.additionView.circle.cornerRadius = 8
+//        options.tabView.additionView.padding = UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 0)
+//        options.tabView.additionView.backgroundColor = #colorLiteral(red: 0.6156862745, green: 0.8039215686, blue: 0.168627451, alpha: 1)
         
-        swipeMenuView.reloadData(options: options)
+//        swipeMenuView.reloadData(options: options)
+//
+//        if let revealController = self.revealViewController() {
+//            revealController.panGestureRecognizer()
+//            revealController.tapGestureRecognizer()
+//            self.navigationItem.leftBarButtonItem?.target = revealController
+//            self.navigationItem.leftBarButtonItem?.action = #selector(SWRevealViewController.revealToggle(_:))
+//        }
         
-        if let revealController = self.revealViewController() {
-            revealController.panGestureRecognizer()
-            revealController.tapGestureRecognizer()
-            self.navigationItem.leftBarButtonItem?.target = revealController
-            self.navigationItem.leftBarButtonItem?.action = #selector(SWRevealViewController.revealToggle(_:))
-        }
     }
     
-    // MARK: - SwipeMenuViewDelegate
-    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, viewWillSetupAt currentIndex: Int) {
-        super.swipeMenuView(swipeMenuView, viewWillSetupAt: currentIndex)
-    }
-    
-    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, viewDidSetupAt currentIndex: Int) {
-        super.swipeMenuView(swipeMenuView, viewDidSetupAt: currentIndex)
-    }
-    
-    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, willChangeIndexFrom fromIndex: Int, to toIndex: Int) {
-        super.swipeMenuView(swipeMenuView, willChangeIndexFrom: fromIndex, to: toIndex)
-    }
-    
-    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, didChangeIndexFrom fromIndex: Int, to toIndex: Int) {
-        super.swipeMenuView(swipeMenuView, didChangeIndexFrom: fromIndex, to: toIndex)
-    }
-    
-    
-    // MARK - SwipeMenuViewDataSource
-    override func numberOfPages(in swipeMenuView: SwipeMenuView) -> Int {
-        return categoryMenuList.count
-    }
-    
-    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, titleForPageAt index: Int) -> String {
-        return children[index].title ?? ""
-    }
-    
-    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, viewControllerForPageAt index: Int) -> UIViewController {
-        let vc = children[index]
-        vc.didMove(toParent: self)
-        return vc
-    }
+//    // MARK: - SwipeMenuViewDelegate
+//    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, viewWillSetupAt currentIndex: Int) {
+//        super.swipeMenuView(swipeMenuView, viewWillSetupAt: currentIndex)
+//    }
+//
+//    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, viewDidSetupAt currentIndex: Int) {
+//        super.swipeMenuView(swipeMenuView, viewDidSetupAt: currentIndex)
+//    }
+//
+//    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, willChangeIndexFrom fromIndex: Int, to toIndex: Int) {
+//        super.swipeMenuView(swipeMenuView, willChangeIndexFrom: fromIndex, to: toIndex)
+//    }
+//
+//    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, didChangeIndexFrom fromIndex: Int, to toIndex: Int) {
+//        super.swipeMenuView(swipeMenuView, didChangeIndexFrom: fromIndex, to: toIndex)
+//    }
+//
+//
+//    // MARK - SwipeMenuViewDataSource
+//    override func numberOfPages(in swipeMenuView: SwipeMenuView) -> Int {
+//        return categoryMenuList.count
+//    }
+//
+//    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, titleForPageAt index: Int) -> String {
+//        return children[index].title ?? ""
+//    }
+//
+//    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, viewControllerForPageAt index: Int) -> UIViewController {
+//        let vc = children[index]
+//        vc.didMove(toParent: self)
+//        return vc
+//    }
 }
