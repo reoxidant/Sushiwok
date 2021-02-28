4e78b9063e7e17a4aafae08e9cdc2b1882378bb1
Vitaliy Shapovalov <kupper133@gmail.com>
2021-02-28 03:00:08 +0300

    update #23 issue
    - add correct table views, but need to fix cotegory menu items

--- Sushiwok/Controllers/MenuListScreen.swift
+++ Sushiwok/Controllers/MenuListScreen.swift
@@ -10,124 +10,24 @@ import UIKit
 import Parchment
 import SWRevealViewController
 
-class HeaderPagingView: PagingView{
-    
-    static let HeaderHeight: CGFloat = 200
-    
-    var headerHeightConstraint: NSLayoutConstraint?
-    
-    private lazy var headerView: UIImageView = {
-        let view = UIImageView(image: UIImage())
-        view.contentMode = .scaleAspectFill
-        return view
-    }
-    
-}
-
 class MenuListScreen: UIViewController {
     
     @IBOutlet weak var menuBtn: UIBarButtonItem!
     
     private let categoryMenuList = ["Наборы и комбо", "Роллы", "Wok", "Суши", "Pizza"]
     
-    var viewControllers = [UIViewController]()
-    
     override func viewDidLoad() {
-        
-        for (index, categoryTitle) in categoryMenuList.enumerated() {
-            let vc = ContentMenuViewController()
-            vc.tableViewPage = index
-            vc.title = categoryTitle
-            viewControllers.append(vc)
-        }
-        
         super.viewDidLoad()
         
         let pagingViewController = PagingViewController()
         pagingViewController.dataSource = self
-        pagingViewController.select(index: 0)
+        pagingViewController.sizeDelegate = self
         
-//        addChild(pagingViewController)
-//        view.addSubview(pagingViewController.view)
-//        pagingViewController.didMove(toParent: self)
-//        pagingViewController.view.translatesAutoresizingMaskIntoConstraints = false
-//
-//        pagingViewController.dataSource = self
-//        pagingViewController.select(index: 0)
-//
-        NSLayoutConstraint.activate([
-            pagingViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            pagingViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            pagingViewController.view.topAnchor.constraint(equalTo: view.topAnchor),
-            pagingViewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
-
-
-        
-        //        NSLayoutConstraint.activate([
-        //            pagingViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-        //            pagingViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-        //            pagingViewController.view.topAnchor.constraint(equalTo: view.topAnchor),
-        //            pagingViewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        //        ])
-        
-        
-        //        var options: SwipeMenuViewOptions = .init()
-        //
-        //        options.tabView.addition = .circle
-        //        options.tabView.margin = 8
-        //        options.tabView.height = 45
-        //        options.tabView.itemView.selectedTextColor = .white
-        //        options.tabView.itemView.font = UIFont.systemFont(ofSize: 16)
-        //        options.tabView.itemView.margin = 8
-        //        options.tabView.itemView.textColor = .black
-        //        options.tabView.additionView.circle.cornerRadius = 8
-        //        options.tabView.additionView.padding = UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 0)
-        //        options.tabView.additionView.backgroundColor = #colorLiteral(red: 0.6156862745, green: 0.8039215686, blue: 0.168627451, alpha: 1)
-        
-        //        swipeMenuView.reloadData(options: options)
-        //
-        //        if let revealController = self.revealViewController() {
-        //            revealController.panGestureRecognizer()
-        //            revealController.tapGestureRecognizer()
-        //            self.navigationItem.leftBarButtonItem?.target = revealController
-        //            self.navigationItem.leftBarButtonItem?.action = #selector(SWRevealViewController.revealToggle(_:))
-        //        }
-        
+        addChild(pagingViewController)
+        view.addSubview(pagingViewController.view)
+        view.constrainToEdges(pagingViewController.view)
+        pagingViewController.didMove(toParent: self)
     }
-    
-    //    // MARK: - SwipeMenuViewDelegate
-    //    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, viewWillSetupAt currentIndex: Int) {
-    //        super.swipeMenuView(swipeMenuView, viewWillSetupAt: currentIndex)
-    //    }
-    //
-    //    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, viewDidSetupAt currentIndex: Int) {
-    //        super.swipeMenuView(swipeMenuView, viewDidSetupAt: currentIndex)
-    //    }
-    //
-    //    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, willChangeIndexFrom fromIndex: Int, to toIndex: Int) {
-    //        super.swipeMenuView(swipeMenuView, willChangeIndexFrom: fromIndex, to: toIndex)
-    //    }
-    //
-    //    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, didChangeIndexFrom fromIndex: Int, to toIndex: Int) {
-    //        super.swipeMenuView(swipeMenuView, didChangeIndexFrom: fromIndex, to: toIndex)
-    //    }
-    //
-    //
-    //    // MARK - SwipeMenuViewDataSource
-    //    override func numberOfPages(in swipeMenuView: SwipeMenuView) -> Int {
-    //        return categoryMenuList.count
-    //    }
-    //
-    //    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, titleForPageAt index: Int) -> String {
-    //        return children[index].title ?? ""
-    //    }
-    //
-    //    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, viewControllerForPageAt index: Int) -> UIViewController {
-    //        let vc = children[index]
-    //        vc.didMove(toParent: self)
-    //        return vc
-    //    }
 }
 
 extension MenuListScreen: PagingViewControllerDataSource{
@@ -136,11 +36,36 @@ extension MenuListScreen: PagingViewControllerDataSource{
     }
     
     func pagingViewController(_: PagingViewController, viewControllerAt index: Int) -> UIViewController {
-        let viewController:UIViewController? = viewControllers[index]
-        return viewController ?? UIViewController()
+        let vc = ContentMenuViewController()
+        vc.tableViewPage = index
+        vc.title = categoryMenuList[index]
+        return vc
     }
     
     func pagingViewController(_: PagingViewController, pagingItemAt index: Int) -> PagingItem {
         return PagingIndexItem(index: index, title: categoryMenuList[index])
+    }
+}
+
+extension MenuListScreen: PagingViewControllerSizeDelegate{
+    func pagingViewController(_ pagingViewController : PagingViewController, widthForPagingItem pagingItem: PagingItem, isSelected: Bool) -> CGFloat {
+        guard let item = pagingItem as? PagingIndexItem else {return 0}
+        
+        let insert = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        let size = CGSize(width: CGFloat.greatestFiniteMagnitude, height: pagingViewController.options.menuItemSize.height)
+        let attr = [NSAttributedString.Key.font: pagingViewController.options.font]
+        
+        let rect = item.title.boundingRect(with: size,
+                                           options: .usesLineFragmentOrigin,
+                                           attributes: attr,
+                                           context: nil)
+        
+        let width = ceil(rect.width) + insert.left + insert.right
+        
+        if isSelected {
+            return width * 1.5
+        }else {
+            return width
+        }
     }
 }
