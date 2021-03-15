f524f52b09d559e3883b6a3723880b217394dac8
Vitaliy Shapovalov <kupper133@gmail.com>
2021-03-16 01:26:47 +0300

    rewrite product data for setting favorite value

--- Sushiwok/Controllers/MenuScreen/ProductViewController.swift
+++ Sushiwok/Controllers/MenuScreen/ProductViewController.swift
@@ -23,16 +23,23 @@ class ProductViewController: UIViewController {
     
     var product: Product? = nil
     
-    @IBAction func votePressed() {
-        product?.isFavorite = !product!.isFavorite
+    var tableViewPage:Int = 0
     
-        if product!.isFavorite {
-            favoriteProductButton.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
-            favoriteProductButton.setTitle("В избраном", for: .normal)
-        } else {
-            favoriteProductButton.backgroundColor = #colorLiteral(red: 0.6689890623, green: 0.8319805861, blue: 0.02307102457, alpha: 1)
-            favoriteProductButton.setTitle("Добавить в избранное", for: .normal)
+    var isFavorite: Bool = false {
+        didSet {
+            if isFavorite == true{
+                favoriteProductButton.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
+                favoriteProductButton.setTitle("В избраном", for: .normal)
+            } else {
+                favoriteProductButton.backgroundColor = #colorLiteral(red: 0.6689890623, green: 0.8319805861, blue: 0.02307102457, alpha: 1)
+                favoriteProductButton.setTitle("Добавить в избранное", for: .normal)
+            }
         }
+    }
+    
+    @IBAction func votePressed() {
+        isFavorite = !product!.isFavorite
+        ProductApi.shared.getProductBy(page: tableViewPage, productId: product!.id)
     }
     
     @IBAction func didTapAddToCardButton() {
@@ -79,6 +86,8 @@ class ProductViewController: UIViewController {
         productPriceLabel.text = "\(productPrice!) ₽"
         
         quantityProduct.text = "\(defaultQuantity)"
+        
+        isFavorite = product!.isFavorite
     }
     
     private func displayProductInfo(){
